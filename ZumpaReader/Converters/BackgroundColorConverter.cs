faf2728e0129049225f6cdf9a10a29dbdac37665
Joe Scurab <joe@scurab.com>
2013-10-06 13:06:22 +0100

    Add simple background converter

---
+++ ZumpaReader/Converters/BackgroundColorConverter.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace ZumpaReader.Converters
+{
+	public class BackgroundColorConverter : IValueConverter
+	{
+        public interface IGetIndexEvaluator
+        {
+            int GetIndex(object o);
+        }
+
+        public BackgroundColorConverter()
+        {
+
+        }
+
+		public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+		{
+            IGetIndexEvaluator indexer = parameter as IGetIndexEvaluator;
+            if (indexer == null)
+            {
+                return new SolidColorBrush(Colors.LightGray);
+            }
+
+            int index = indexer.GetIndex(value);
+            if (index % 2 == 0)
+            {
+                return new SolidColorBrush(Colors.LightGray);
+            }
+            else
+            {
+                return new SolidColorBrush(Colors.DarkGray);
+            }
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+		{
+			return null;
+		}
+	}
+}
