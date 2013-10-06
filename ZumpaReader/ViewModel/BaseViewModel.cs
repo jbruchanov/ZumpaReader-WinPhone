1227faf9d2ad6cd9adbaea57b99c599e719a4c35
Joe Scurab <joe@scurab.com>
2013-10-06 18:43:44 +0100

    Add Page property into baseview model and add binding

--- ZumpaReader/ViewModel/BaseViewModel.cs
+++ ZumpaReader/ViewModel/BaseViewModel.cs
@@ -1,14 +1,29 @@
-﻿using System;
+﻿using Microsoft.Phone.Controls;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
+using System.Windows;
 
 namespace ZumpaReader.ViewModel
 {
-    public class BaseViewModel : INotifyPropertyChanged
+    public class BaseViewModel : DependencyObject, INotifyPropertyChanged
     {
+
+        public PhoneApplicationPage Page
+        {
+            get { return (PhoneApplicationPage)GetValue(PageProperty); }
+            set { SetValue(PageProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for ParentView.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty PageProperty =
+            DependencyProperty.Register("Page", typeof(PhoneApplicationPage), typeof(BaseViewModel), null);
+
+        
+
         public void NotifyPropertyChange([CallerMemberName] String propertyName = "")
         {
             if (PropertyChanged != null)
