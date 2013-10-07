bd6c45834a17c7238c28f9486d97a67565793f71
Joe Scurab <joe@scurab.com>
2013-10-08 00:42:53 +0100

    Add more model classes

---
+++ ZumpaReader/Model/Survey.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ZumpaReader.Model
+{
+    public class Survey
+    {
+        [JsonProperty("Question")]
+        public string Question { get; set; }
+
+        [JsonProperty("Responds")]
+        public int Responds { get; set; }
+
+        [JsonProperty("ID")]
+        public int ID { get; set; }
+
+        [JsonProperty("Answers")]
+        public List<string> Answers { get; set; }
+
+        [JsonProperty("Percents")]
+        public List<int> Percents { get; set; }
+
+        [JsonProperty("VotedItem")]
+        public int VotedItem { get; set; }
+
+        public Survey()
+        {
+            VotedItem = -1;
+        }
+    }
+}
---
+++ ZumpaReader/Model/ZumpaSubItem.cs
@@ -0,0 +1,41 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ZumpaReader.Model
+{
+    public class ZumpaSubItem
+    {
+        [JsonProperty("ID")]
+        public int ID { get; set; }
+
+        [JsonProperty("ParentID")]
+        public int ParentID { get; set; }
+
+        [JsonProperty("AuthorReal")]
+        public string AuthorReal { get; set; }
+
+        [JsonProperty("AuthorFake")]
+        public string AuthorFake { get; set; }
+
+        [JsonProperty("Body")]
+        public string Body { get; set; }
+
+        [JsonProperty("Time")]
+        public long Time { get; set; }
+
+        [JsonProperty("HasRespondForYou")]
+        public bool HasRespondForYou { get; set; }
+
+        [JsonProperty("HasInsideUris")]
+        public bool HasInsideUris { get; set; }
+
+        [JsonProperty("InsideUris")]
+        public List<String> InsideUris { get; set; }
+
+        [JsonProperty("Survey")]
+        public Survey Survey { get; set; }
+    }
+}
