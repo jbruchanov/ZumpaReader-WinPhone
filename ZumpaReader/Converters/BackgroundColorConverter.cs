02bbc2bf64854be95771e65445416bdde4a3bfdc
Joe Scurab <joe@scurab.com>
2013-10-07 21:20:11 +0100

    Code format

--- ZumpaReader/Converters/BackgroundColorConverter.cs
+++ ZumpaReader/Converters/BackgroundColorConverter.cs
@@ -7,8 +7,8 @@ using System.Windows.Media;
 
 namespace ZumpaReader.Converters
 {
-	public class BackgroundColorConverter : IValueConverter
-	{
+    public class BackgroundColorConverter : IValueConverter
+    {
 
         private Brush _even;
         private Brush _odd;
@@ -24,19 +24,19 @@ namespace ZumpaReader.Converters
             _odd = App.Current.Resources["RowBackgroundOdd"] as Brush;
         }
 
-		public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
-		{
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
             IGetIndexEvaluator indexer = parameter as IGetIndexEvaluator;
             if (indexer == null)
                 return _even;
 
             int index = indexer.GetIndex(value);
-            return (index % 2 == 0) ? _even : _odd;            
-		}
+            return (index % 2 == 0) ? _even : _odd;
+        }
 
-		public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
-		{
-			return null;
-		}
-	}
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return null;
+        }
+    }
 }
