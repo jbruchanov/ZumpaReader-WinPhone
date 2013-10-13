5486df8afd84e86754778f53b70508486dce4072
Joe Scurab <joe@scurab.com>
2013-10-13 22:25:02 +0100

    Add app title to resources

--- ZumpaReader/Resources/Labels.Designer.cs
+++ ZumpaReader/Resources/Labels.Designer.cs
@@ -61,6 +61,15 @@ namespace ZumpaReader.Resources {
         }
         
         /// <summary>
+        ///   Looks up a localized string similar to Žumπčka.
+        /// </summary>
+        public static string AppName {
+            get {
+                return ResourceManager.GetString("AppName", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized string similar to General.
         /// </summary>
         public static string General {
