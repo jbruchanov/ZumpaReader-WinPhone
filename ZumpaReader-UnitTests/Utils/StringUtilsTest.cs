d34f999a2c8509d54d03d3b59ab4bf7c7322e63b
Joe Scurab <joe@scurab.com>
2013-10-14 22:34:04 +0100

    Add string utils

---
+++ ZumpaReader-UnitTests/Utils/StringUtilsTest.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZumpaReader;
+using ZumpaReader.Model;
+using ZumpaReader.Utils;
+
+namespace ZumpaReader_UnitTests.Utils
+{
+    [TestClass]
+    public class StringUtilsTest : TestCase
+    {
+        [TestMethod]
+        public void TestExtractID()
+        {
+            string url = "http://portal2.dkm.cz/phorum/read.php?f=2&i=1225378&t=1225378";
+            string expected = "1225378";
+            string value = StringUtils.ExtractThreadId(url);
+
+            Assert.AreEqual(expected, value);
+        }
+
+        [TestMethod]
+        public void TestExtractIDNotLast()
+        {
+            string url = "http://portal2.dkm.cz/phorum/read.php?f=2&i=1225378&t=1225378&q=b";
+            string expected = "1225378";
+            string value = StringUtils.ExtractThreadId(url);
+
+            Assert.AreEqual(expected, value);
+        }
+    }
+}
---
+++ ZumpaReader/Utils/StringUtils.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ZumpaReader.Utils
+{
+    public class StringUtils
+    {
+        private static string THREAD_ID_PREFIX = "&t=";
+        private static string AMPERSAND = "&";
+
+        public static string ExtractThreadId(string threadUrl)
+        {
+            int start = threadUrl.IndexOf("&t=") + THREAD_ID_PREFIX.Length;
+            int end = threadUrl.IndexOf(AMPERSAND, start);            
+            return end > -1 ? threadUrl.Substring(start, end - start) : threadUrl.Substring(start);
+        }
+    }
+}
