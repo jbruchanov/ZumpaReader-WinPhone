72c329f06bc2c5ae40f924b46b4b2df41670e4e9
Joe Scurab <joe@scurab.com>
2013-10-19 19:50:41 +0100

    Add left strip color

---
+++ ZumpaReader/Converters/StripColorConverter.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+using System.Windows.Media;
+using ZumpaReader.Model;
+
+namespace ZumpaReader.Converters
+{
+    public class StripColorConverter : IValueConverter
+    {
+
+        private SolidColorBrush _transparent = new SolidColorBrush(Colors.Transparent);
+
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            ZumpaItem item = value as ZumpaItem;
+            if (String.Equals(item.Author, AppSettings.Login))
+            {
+                return App.Current.Resources["StripOwnThread"];
+            }
+            else if (item.IsFavourite)
+            {
+                return App.Current.Resources["StripFavThread"];
+            }
+            else if (item.HasResponseForYou)
+            {
+                return App.Current.Resources["StripResponseThread"];
+            }
+            else
+            {
+                return _transparent;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
